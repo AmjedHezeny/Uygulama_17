fafc953fa512e57bb41cab567b711f6eb86a73b7
amged <amged@DESKTOP-9MT6K87>
2023-08-23 22:04:04 +0300

    Add project files.

---
+++ Uygulama_17/Metodlar.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uygulama_17
+{
+    internal class Metodlar
+    {
+        public static void Listele(string[] isimler)
+        {
+            Console.WriteLine("*********************");
+            Console.WriteLine("Listelemiş İsimler.");
+            Console.WriteLine("*********************");
+            foreach (var isim in isimler)
+            {
+                Console.WriteLine(isim);
+            }
+            Console.WriteLine();
+            Console.WriteLine("*********************");
+            Console.WriteLine();
+        }
+                //-------------------------------------------------
+
+        public static void KisiSayisi(string[] isimler)
+        {
+            Console.WriteLine("*********************");
+            Console.WriteLine($"Kişi Sayısı: {isimler.Length}");
+            Console.WriteLine("*********************");
+            Console.WriteLine();
+        }
+
+        //-------------------------------------------------
+
+        public static void EnUzunIsim(string[] isimler)
+        {
+            string enUzunIsim = "";
+            int enUzunIsimUzunlugu = 0;
+
+            foreach (var isim in isimler)
+            {
+                if (isim.Length > enUzunIsimUzunlugu)
+                {
+                    enUzunIsim = isim;
+                    enUzunIsimUzunlugu = isim.Length;
+                }
+            }
+            Console.WriteLine("*********************");
+            Console.WriteLine($"En Uzun İsimli Öğrenci: {enUzunIsim}");
+            Console.WriteLine("*********************");
+            Console.WriteLine();
+        }
+
+        //-------------------------------------------------
+
+        public static void AlfabetikSiralama(string[] isimler)
+        {
+            Array.Sort(isimler, StringComparer.CurrentCultureIgnoreCase);
+
+            Console.WriteLine("İsimlerin Alfabetik Sıralaması.");
+            Console.WriteLine("*********************");
+            foreach (var isim in isimler)
+            {
+                Console.WriteLine(isim);
+            }
+            Console.WriteLine();
+            Console.WriteLine("*********************");
+            Console.WriteLine();
+        }
+        //-------------------------------------------------
+
+        public static void HarfIleBaslayanIsimler(string[] isimler)
+        {
+            Console.WriteLine("*********************");
+            Console.Write("Başlamasını İstediğiniz Harfi Giriniz: ");
+            char harf = char.ToLower(Console.ReadLine()[0]);
+            Console.WriteLine("*********************");
+            Console.WriteLine("Girilen Harfle Başlayan İsimler.");
+            Console.WriteLine("*********************");
+
+            foreach (var isim in isimler)
+            {
+                if (char.ToLower(isim[0]) == harf)
+                {
+                    Console.WriteLine(isim);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("*********************");
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ Uygulama_17/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uygulama_17
+{
+    internal class Program
+    {
+        static string[] isimler = new string[]
+        {        
+        "Demircan Baydil.",
+        "Burçin Kübra Baykal.",
+        "Derviş Haluk Baykan.",
+        "Taylan Remzi Baykuş.",
+        "Abdulvahap Bayrakoğlu.",
+        "Aygün Bayram.",
+        "Ayla Baytın.",
+        "Kubilay Barış Begiç."
+        };
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Programımıza hoş Geldiniz :)");
+                Console.WriteLine("1).İsimleri Listele.");
+                Console.WriteLine("2).Kişi Sayısı.");
+                Console.WriteLine("3).En Uzun İsimli öğrencinin İsmi.");
+                Console.WriteLine("4).İsimlerin Alfabetik Sıralaması.");
+                Console.WriteLine("5).Girilen Harf İle Başlayan İsimler.");
+                Console.WriteLine("0).Çıkış.");
+
+                Console.Write("Seçiminizi Yapınız: ");
+                char seçme = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                Console.Clear();
+
+                try
+                {
+                    switch (seçme)
+                    {
+                        case '1':
+                            Metodlar.Listele(isimler);
+                            break;
+                        case '2':
+                            Metodlar.KisiSayisi(isimler);
+                            break;
+                        case '3':
+                            Metodlar.EnUzunIsim(isimler);
+                            break;
+                        case '4':
+                            Metodlar.AlfabetikSiralama(isimler);
+                            break;
+                        case '5':
+                            Metodlar.HarfIleBaslayanIsimler(isimler);
+                            break;
+                        case '0':
+                            Console.WriteLine("Programı kullandığınız için teşekkür ederiz. Çıkış için herhangi bir tuşa basınız.");
+                            Console.ReadKey();
+                            return;
+                        default:
+                            Console.WriteLine("Geçersiz bir seçim yaptınız.");
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Bir hata oluştu. Lütfen tekrar deneyin.");
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
